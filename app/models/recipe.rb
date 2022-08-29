ec70a6e6363af93c4d2d7a63300458a179a3325c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-29 20:50:13 +0200

    configure recipe model relationships and referential integrity constraints

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', dependent: :destroy
+  has_many :recipe_foods, class_name: 'recipe_food', foreign_key: 'recipe_id'
 end
