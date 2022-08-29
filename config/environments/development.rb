100f183b6c06d3afa5eb871d646b3446d0b38c61
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-29 21:12:24 +0200

    confure mailer devise and mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
+  LetterOpenerWeb.configure do |config|
+    config.letters_location = Rails.root.join('your', 'new', 'path')
+  end
 end
