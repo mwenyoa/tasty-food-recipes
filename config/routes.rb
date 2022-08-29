7f726c78f84d4269fe211ed0d9b138ef0dad9cb2
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-29 21:07:20 +0200

    Configure routes for devise, letter opener and  default page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/create'
+  get 'foods/index'
+  get 'foods/create'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'foods#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
