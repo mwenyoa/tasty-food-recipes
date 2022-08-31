d5748a9c0ca93fcfa18f4a6d97f0f3c91ce834ac
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-31 15:18:21 +0200

    Update  app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/create'
-  get 'foods/index'
-  get 'foods/create'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :user
   root 'foods#index'
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :new, :show, :create, :destroy]
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
