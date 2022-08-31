549c3ce67dcf47a1f2eabea1f5443cbd22ef692f
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-31 15:09:20 +0200

    Add logic to add, delete and create a  food item

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,36 @@
 class FoodsController < ApplicationController
-  def index; end
-
-  def create; end
-
-  def destroy; end
+  load_and_authorize_resource
+  def index
+    @foods = Food.order(created_at: :desc).limit(5)
+  end
+    
+  def new 
+    @food = Food.new
+    @params = params
+  end
+    
+  def create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      flash['notice'] = 'Food has been added successfully!'
+      redirect_to user_foods_path
+    else
+      flash['alert'] = 'There was an error in adding food item'
+      render :new
+    end
+  end
+    
+    def destroy
+      @food = Food.find(params[:id])
+      @food.destroy
+      flash[:notice] = 'Food deleted successfully'
+      redirect_to request.path
+    end
+    
+    private 
+    
+    def food_params
+      params.require(:food).permit(:name, :measurement_unit, :price)
+    end
 end
+
