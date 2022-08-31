97af1741c632a2b410dd9107d9e3fb6b9e0d9292
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-31 19:59:48 +0200

    Redefine refrential integrity contraints in  Recipe controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
-  belongs_to :user, class_name: 'User', dependent: :destroy
-  has_many :recipe_foods, class_name: 'recipe_food', foreign_key: 'recipe_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id', dependent: :destroy
 end
