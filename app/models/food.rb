7b9f01295c95413ad32629367bbd8ae2babdc62c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-31 15:12:14 +0200

    Add validations for the food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id', dependent: :destroy
   has_many :recipe_foods, class_name: 'recipe_food', foreign_key: 'recipe_id'
+
+  validates :name, presence: true, _uniqueness: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
 end
