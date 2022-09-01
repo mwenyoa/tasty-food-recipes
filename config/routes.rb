e1e8f7af4adde7489085207965bbf290a158f0d6
Lembani <Lembanisakala@gmail.com>
2022-09-01 15:36:14 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root 'foods#index'
   resources :users, only: [:index, :show] do
     resources :foods, only: [:index, :new, :show, :create, :destroy]
+    resources :recipes, only: [:index, :new, :show, :create, :destroy]
   end
+
+  patch '/recipes/:id', to: 'recipes#toggle_public', as: :update_recipe
+
+  resources :recipes, only: [:index, :show]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
