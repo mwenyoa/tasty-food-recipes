c543daf30a725b756f0b53ad74948cd285662821
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-02 19:51:05 +0200

    Add optional test files

---
+++ spec/requests/public_receipes_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "PublicReceipes", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/public_receipes/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/public_receipes/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
