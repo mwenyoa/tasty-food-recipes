bea5bc09751719c556de605131bc5cf6e0c9e547
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-02 19:48:39 +0200

    Add column to  recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_084625) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_02_100910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_084625) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_count", default: 0
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
