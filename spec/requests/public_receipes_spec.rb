0da7ac7de10e75aacd2e9e9476bdf85d77204403
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-02 19:58:27 +0200

    FIx linter errors

--- spec/requests/public_receipes_spec.rb
+++ spec/requests/public_receipes_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "PublicReceipes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/public_receipes/index"
+RSpec.describe 'PublicReceipes', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/public_receipes/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/public_receipes/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/public_receipes/show'
       expect(response).to have_http_status(:success)
     end
   end
