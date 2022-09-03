94e2998bdbbc69ad2e17a61837ae79eb8f16badd
Lembani <Lembanisakala@gmail.com>
2022-09-03 09:22:42 +0200

    Update recipe food methods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,6 +1,12 @@
 class RecipeFoodsController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
+    if @recipe_food.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
 
   def new
@@ -9,6 +15,23 @@ class RecipeFoodsController < ApplicationController
   end
 
   def edit
-    @recipe_food = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    if @recipe_food.update(recipe_food_params)
+      flash[:succes] = 'Recipe food updated successfully'
+      redirect_to recipe_path(@recipe_food.recipe)
+    else
+      flash[:succes] = 'Recipe food update failed'
+      render :edit
+    end
+  end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :food_id)
   end
 end
