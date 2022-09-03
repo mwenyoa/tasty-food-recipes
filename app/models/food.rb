44559146cc32e8df3eb19c5a611da636f25a49a8
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-03 21:03:43 +0200

    Fix systenm features

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id', dependent: :destroy
+  belongs_to :user, class_name: 'User'
+  has_many :recipe_foods, class_name: 'RecipeFood', dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
   validates :name, presence: true, _uniqueness: true
